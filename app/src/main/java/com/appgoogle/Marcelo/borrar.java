e4870897234607485898e51c5730b8a53b2c08e5
Marcelo <marcelooscco1@gmail.com>
2024-09-04 00:22:27 -0500

    vadeuna

--- app/src/main/java/com/appgoogle/Marcelo/borrar.java
+++ app/src/main/java/com/appgoogle/Marcelo/borrar.java
@@ -1,4 +1,40 @@
 package com.appgoogle.Marcelo;
 
-public class borrar {
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.appgoogle.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+
+public class borrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barra); // Asegúrate de que el layout correcto esté vinculado
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.action_explore) {
+                    // Acción para "Explorar"
+                    return true;
+                } else if (id == R.id.action_saved) {
+                    // Acción para "Guardados"
+                    return true;
+                } else if (id == R.id.action_contribute) {
+                    // Acción para "Contribuir"
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
 }
