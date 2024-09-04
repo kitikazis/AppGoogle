9ab4698f3f1e2b591b06788dee1b626f654aadee
kitikaziss <luiskitikazis2@gmail.com>
2024-09-03 23:29:55 -0500

    buscador hecho

--- app/src/main/java/com/appgoogle/Yamir/GoogleActivity.java
+++ app/src/main/java/com/appgoogle/Yamir/GoogleActivity.java
@@ -1,26 +1,75 @@
 package com.appgoogle.Yamir;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.widget.SearchView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.appgoogle.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+
+public class GoogleActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap myMap;
+    private SearchView mapSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_google);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mapSearchView = findViewById(R.id.mapSearch);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (myMap != null) {
+                    String location = mapSearchView.getQuery().toString();
+                    List<Address> addressList = null;
+                    if (location != null && !location.isEmpty()) {
+                        Geocoder geocoder = new Geocoder(GoogleActivity.this);
+                        try {
+                            addressList = geocoder.getFromLocationName(location, 1);
+                            if (addressList != null && !addressList.isEmpty()) {
+                                Address address = addressList.get(0);
+                                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                                myMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                                myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
         });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+        LatLng sydney = new LatLng(-31, 150);
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
