471795e02a411661455ff00c712203da06d74a57
Marcelo <marcelooscco1@gmail.com>
2024-09-05 18:06:13 -0500

    modv3_barra

--- app/src/main/java/com/appgoogle/Yamir/GoogleActivity.java
+++ app/src/main/java/com/appgoogle/Yamir/GoogleActivity.java
@@ -1,75 +1,47 @@
 package com.appgoogle.Yamir;
 
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
-import android.widget.SearchView;
-
+import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.appgoogle.Marcelo.GuardadosFragment;
 import com.appgoogle.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.io.IOException;
-import java.util.List;
-
-public class GoogleActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private GoogleMap myMap;
-    private SearchView mapSearchView;
+public class GoogleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google);
 
-        mapSearchView = findViewById(R.id.mapSearch);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        }
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
 
-        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                if (myMap != null) {
-                    String location = mapSearchView.getQuery().toString();
-                    List<Address> addressList = null;
-                    if (location != null && !location.isEmpty()) {
-                        Geocoder geocoder = new Geocoder(GoogleActivity.this);
-                        try {
-                            addressList = geocoder.getFromLocationName(location, 1);
-                            if (addressList != null && !addressList.isEmpty()) {
-                                Address address = addressList.get(0);
-                                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                                myMap.addMarker(new MarkerOptions().position(latLng).title(location));
-                                myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-                return false;
+            if (itemId == R.id.navigation_explore) {
+                // Acción para el botón "Explorar"
+                return true;
+            } else if (itemId == R.id.navigation_saved) {
+                selectedFragment = new GuardadosFragment();
+            } else if (itemId == R.id.navigation_contribute) {
+
+                finish(); // Cerrar la aplicación
+                return true;
             }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .addToBackStack(null)
+                        .commit();
+                return true;
             }
+            return false;
         });
-    }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        myMap = googleMap;
-        LatLng sydney = new LatLng(-31, 150);
-        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
