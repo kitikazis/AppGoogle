731b8c57d5c421277b86e856ca5eb3cffe04d566
Marcelo <marcelooscco1@gmail.com>
2024-09-05 18:34:16 -0500

    modv4_barrainf

--- app/src/main/java/com/appgoogle/Yamir/GoogleActivity.java
+++ app/src/main/java/com/appgoogle/Yamir/GoogleActivity.java
@@ -3,9 +3,9 @@ package com.appgoogle.Yamir;
 import android.os.Bundle;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import com.appgoogle.Marcelo.GuardadosFragment;
 import com.appgoogle.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -24,13 +24,13 @@ public class GoogleActivity extends AppCompatActivity {
             int itemId = item.getItemId();
 
             if (itemId == R.id.navigation_explore) {
-                // Acción para el botón "Explorar"
+                getSupportFragmentManager().popBackStack(null, getSupportFragmentManager().POP_BACK_STACK_INCLUSIVE);
                 return true;
             } else if (itemId == R.id.navigation_saved) {
                 selectedFragment = new GuardadosFragment();
             } else if (itemId == R.id.navigation_contribute) {
-
-                finish(); // Cerrar la aplicación
+                // Mostrar el diálogo de confirmación para salir de la aplicación
+                showExitConfirmationDialog();
                 return true;
             }
 
@@ -43,5 +43,15 @@ public class GoogleActivity extends AppCompatActivity {
             }
             return false;
         });
+    }
+
+    // Método para mostrar el diálogo de confirmación de salida
+    private void showExitConfirmationDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Salir de la aplicación")
+                .setMessage("¿Estás seguro de que deseas salir?")
+                .setPositiveButton("Sí", (dialog, which) -> finish()) // Cierra la aplicación
+                .setNegativeButton("No", (dialog, which) -> dialog.dismiss()) // Cierra el diálogo y continúa la ejecución
+                .show();
     }
 }
